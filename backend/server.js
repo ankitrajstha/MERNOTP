61fe306ff49bb56e77484f048a40ad94bbe096e6
Ankit Shrestha <ankitrajstha>
2024-09-01 21:46:11 +0545

    feat: add route in server

--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use("/api", verificationRoutes);
+
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
