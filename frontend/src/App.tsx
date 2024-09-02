7866fdba5cdd09e070f18e15f63fdd2bccd98f7e
Ankit Shrestha <ankitrajstha>
2024-09-02 11:55:55 +0545

    feat: add page not found

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Otp from "./pages/Otp";
+import PageNotFound from "./pages/PageNotFound";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Otp />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
